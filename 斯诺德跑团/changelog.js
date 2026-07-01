401c89c3a9930fc0702db82d375bf5a2741f251b
Doylesama114 <m13194938928@gmail.com>
2026-07-01 21:37:48 +0800

    bump: v1.0.6013

--- 斯诺德跑团/changelog.js
+++ 斯诺德跑团/changelog.js
@@ -2,6 +2,15 @@
 var SNOWD_CHANGELOG = [
 
   {
+    version: '1.0.6013',
+    date: '2026-07-01',
+    changes: [
+      '账号系统+151卡池+自定义牌组+注册/组牌UI'
+    ]
+  },
+
+
+  {
     version: '1.0.7000',
     date: '2026-07-01',
     changes: [
